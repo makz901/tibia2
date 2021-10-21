9e54a97af83fb1a5b8925dde490a8779a3b42a98
makz <maksym.krawczyk@gmail.com>
2021-10-21 22:20:47 +0200

    Testing lib

--- Team801.Tibia2.Client/Program.cs
+++ Team801.Tibia2.Client/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
 
 namespace Team801.Tibia2.Client
 {
     class Program
     {
+        private static NetPeer _server;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Client!");
+
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager client = new NetManager(listener);
+            client.Start();
+            client.Connect("localhost" /* host ip or name */, 9050 /* port */, "SomeConnectionKey" /* text key or NetDataWriter */);
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+            {
+                Console.WriteLine($"We got: '{dataReader.GetString()}' from {fromPeer}");
+                dataReader.Recycle();
+
+                _server = fromPeer;
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                client.PollEvents();
+                Thread.Sleep(15);
+
+                var test = Console.ReadLine();
+                var writer = new NetDataWriter();
+                writer.Put(test);
+                _server?.Send(writer, DeliveryMethod.Unreliable);
+            }
+
+            client.Stop();
         }
     }
 }
--- Team801.Tibia2.Server/Program.cs
+++ Team801.Tibia2.Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
 
 namespace Team801.Tibia2.Server
 {
@@ -7,6 +10,39 @@ namespace Team801.Tibia2.Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Server!");
+
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(9050 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if(server.ConnectedPeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.Unreliable);             // Send with reliability
+            };
+
+            listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+            {
+                Console.WriteLine("Server got: {0}", dataReader.GetString(100 /* max length of string */));
+                dataReader.Recycle();
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
         }
     }
 }
