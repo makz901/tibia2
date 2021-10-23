9dee1a5575c8d27b3c575a30eb7e3d1c2bd000f1
Maksym Krawczyk <maksym.krawczyk@gmail.com>
2021-10-23 10:22:50 +0200

    added movement example in console

--- Team801.Tibia2.Client/Program.cs
+++ Team801.Tibia2.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using UnityEngine;
 
 namespace Team801.Tibia2.Client
 {
@@ -10,11 +11,37 @@ namespace Team801.Tibia2.Client
             var client = new Client();
             client.Connect("makz");
 
-            //Simulating game updates
-            while (!Console.KeyAvailable)
+            ConsoleKeyInfo info;
+
+            do {
+                while (!Console.KeyAvailable)
+                {
+                    client.OnFrameUpdated();
+                    Thread.Sleep(15);
+                }
+
+                info = Console.ReadKey(true);
+                HandleKey(info.Key, client);
+
+            } while(info.Key != ConsoleKey.Backspace);
+        }
+
+        private static void HandleKey(ConsoleKey key, Client client)
+        {
+            switch (key)
             {
-                client.OnFrameUpdated();
-                Thread.Sleep(15);
+                case ConsoleKey.UpArrow:
+                    client.Move(Vector2.up);
+                    break;
+                case ConsoleKey.DownArrow:
+                    client.Move(Vector2.down);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    client.Move(Vector2.left);
+                    break;
+                case ConsoleKey.RightArrow:
+                    client.Move(Vector2.right);
+                    break;
             }
         }
     }
