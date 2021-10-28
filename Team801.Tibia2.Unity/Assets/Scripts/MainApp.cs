d4c18524003b9358018865f0a2ff261744155309
Maksym Krawczyk <maksym.krawczyk@gmail.com>
2021-10-28 12:58:28 +0200

    Moved reordered projects

--- Team801.Tibia2.Unity/Assets/Scripts/MainApp.cs
+++ Team801.Tibia2.Unity/Assets/Scripts/MainApp.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Team801.Tibia2.Client;
 
 public class MainApp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        var client = new Client();
+        client.Connect("mkz unity");
     }
 
     // Update is called once per frame
